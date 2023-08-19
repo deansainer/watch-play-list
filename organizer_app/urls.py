5dc7d6dc6636045df10e18d057adb0ba840aa0c3
deansainer <determind2k@gmail.com>
2023-08-19 12:51:15 +0200

    added navbar

--- organizer_app/urls.py
+++ organizer_app/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', views.MovieView.as_view()),
+    path('', views.MovieView.as_view(), name='home_url'),
     path('delete/<str:id>', views.delete, name='delete_url'),
     path('details/<str:id>', views.details, name='details_url')
 ]
