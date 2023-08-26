966d344165e4c0c6ad82e7ddd03901a179a9f7bb
deansainer <determind2k@gmail.com>
2023-08-26 13:40:12 +0200

    added login, register, logout functionality

--- organizer_app/urls.py
+++ organizer_app/urls.py
@@ -4,11 +4,15 @@ from .views import *
 
 
 urlpatterns = [
+    path('register/', views.register, name='register_url'),
+    path('login/', views.user_login, name='login_url'),
+    path('logout/', views.user_logout, name='logout_url'),
     path('', views.MovieView.as_view(), name='home_url'),
     path('delete/<str:id>', views.delete, name='delete_url'),
     path('details/<str:id>', views.details, name='details_url'),
     path('mark_as_watched/<str:id>', views.mark_as_watched, name='mark_as_watched_url'),
     path('history', views.HistoryView.as_view(), name='history_url'),
     path('mark_as_unwatched/<str:id>', views.mark_as_unwatched, name='mark_as_unwatched_url'),
-    path('most_popular_movies', views.MostPopularMoviesView.as_view(), name = 'most_popular_movies_url')
+    path('most_popular_movies', views.MostPopularMoviesView.as_view(), name = 'most_popular_movies_url'),
+
 ]
--- organizer_app/views.py
+++ organizer_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView
 import requests
@@ -17,61 +19,96 @@ most_popular_movies_querystring = {"currentCountry":"US","purchaseCountry":"US",
 content_data_url = "https://online-movie-database.p.rapidapi.com/auto-complete"
 content_details_url = "https://online-movie-database.p.rapidapi.com/title/get-overview-details"
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
+    return render(request, 'organizer_app/register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'organizer_app/login.html')
+    else:
+        return render(request, 'organizer_app/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
+
+
 class MovieView(View):
     def get(self, request, *args, **kwargs):
-        content_list = Content.objects.filter(is_watched=False)
-        return render(request,'organizer_app/organizer.html', {'content_list': content_list})
+        if request.user.is_authenticated:
+            content_list = Content.objects.filter(is_watched=False)
+            return render(request,'organizer_app/organizer.html', {'content_list': content_list})
+        else:
+            return redirect('login_url')
     def post(self, request, *args, **kwargs):
         # getting all unwatched objects from 'content' model
-        content_list = Content.objects.filter(is_watched=False)
-        try:
-            # get movie name from user
-            query = ''
-            if 'get_movie' in request.POST:
-                query= request.POST.get('movie_name')
-                querystring = {"q": query}
+        if request.user.is_authenticated:
+            content_list = Content.objects.filter(is_watched=False)
+            try:
+                # get movie name from user
+                query = ''
+                if 'get_movie' in request.POST:
+                    query= request.POST.get('movie_name')
+                    querystring = {"q": query}
 
-                #get json data of entered movie name
-                response = requests.get(content_data_url, headers=headers, params=querystring).json()
+                    #get json data of entered movie name
+                    response = requests.get(content_data_url, headers=headers, params=querystring).json()
 
-                #get id of entered movie
-                get_id = response['d'][0]['id']
+                    #get id of entered movie
+                    get_id = response['d'][0]['id']
 
-                querystring = {"tconst": get_id}
+                    querystring = {"tconst": get_id}
 
-            if 'get_movie_url' in request.POST:
-                get_url = request.POST.get('movie_url')
-                match = re.search(r'tt\d+', get_url)
-                if match:
-                    get_id = match.group(0)
+                if 'get_movie_url' in request.POST:
+                    get_url = request.POST.get('movie_url')
+                    match = re.search(r'tt\d+', get_url)
+                    if match:
+                        get_id = match.group(0)
 
-                querystring = {"tconst": get_id}
+                    querystring = {"tconst": get_id}
 
-            content_details_response = requests.get(content_details_url, headers=headers, params=querystring).json()
+                content_details_response = requests.get(content_details_url, headers=headers, params=querystring).json()
 
-            content_details_data = {
-                'id': get_id,
-                'title': content_details_response['title']['title'],
-                'year': content_details_response['title']['year'],
-                'type': content_details_response['title']['titleType'].capitalize(),
-                'top_rank': content_details_response['ratings']['topRank'],
-                'image': content_details_response['title']['image']['url'],
-                'duration': content_details_response['title']['runningTimeInMinutes'],
-                'rating': content_details_response['ratings']['rating'],
-                'genres': ', '.join(content_details_response['genres']),
-                'some_plot': content_details_response['plotOutline']['text'],
-                'full_plot': content_details_response['plotSummary']['text'],
-            }
+                content_details_data = {
+                    'id': get_id,
+                    'title': content_details_response['title']['title'],
+                    'year': content_details_response['title']['year'],
+                    'type': content_details_response['title']['titleType'].capitalize(),
+                    'top_rank': content_details_response['ratings']['topRank'],
+                    'image': content_details_response['title']['image']['url'],
+                    'duration': content_details_response['title']['runningTimeInMinutes'],
+                    'rating': content_details_response['ratings']['rating'],
+                    'genres': ', '.join(content_details_response['genres']),
+                    'some_plot': content_details_response['plotOutline']['text'],
+                    'full_plot': content_details_response['plotSummary']['text'],
+                }
 
-            #save data from json to model
-            Content.objects.get_or_create(**content_details_data)
-            redirect('/')
+                #save data from json to model
+                Content.objects.get_or_create(**content_details_data)
+                redirect('/')
 
-            return render(request,'organizer_app/organizer.html', {'content_list': content_list})
-        except Exception:
-            messages.info(request, 'Content not found, try again.')
-            return render(request, 'organizer_app/organizer.html',{'content_list': content_list})
-
+                return render(request,'organizer_app/organizer.html', {'content_list': content_list})
+            except Exception:
+                messages.info(request, 'Content not found, try again.')
+                return render(request, 'organizer_app/organizer.html',{'content_list': content_list})
+        else:
+            return render(request, 'organizer_app/login.html')
 # delete content from list
 def delete(request, id):
     content = Content.objects.get(id=id)
